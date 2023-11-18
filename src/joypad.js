fae92fe836aebc2b0a82e392107aaae084a30682
HenryDalrymple53 <henrydalrymple53@gmail.com>
2023-11-17 18:49:54 -0800

    fasefasef

--- src/joypad.js
+++ src/joypad.js
@@ -1,5 +1,4 @@
 // Joypad library
-/*
 import emitter from './emitter';
 import { EVENTS, AXIS_MOVEMENT_THRESHOLD } from './constants';
 import { log, hasVibrationSupport } from './helpers';
@@ -58,4 +57,3 @@ const joypad = {
 };
 
 export default joypad;
-*/
